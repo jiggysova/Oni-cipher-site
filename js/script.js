9a1969ef7b44813ff00a678c4a87c35ff8193bd3
Jeremiah Pusey Lawrence <jiggyempires@icloud.com>
2025-09-08 14:12:57 -0400

    Third Commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Typewriter effect
+const text = "Websites That Command Respect."
+let i = 0;
+
+function typeWriter() {
+    if (i < text.length) {
+        document.getElementById("hero-text").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, 100);
+    }
+}
+
+window.onload = typeWriter;
